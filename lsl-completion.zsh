5a4e966b61f84c9d8ca3ebbc08489bafb0c46d64
Johan Medrano <jmedrano@lirmm.fr>
2021-12-28 17:35:48 +0100

    refactor

--- lsl-completion.zsh
+++
@@ -1,5 +0,0 @@
-autoload -U +X compinit && compinit
-autoload -U +X bashcompinit && bashcompinit
-
-CURDIR=`cd $(dirname $0); pwd`
-source $CURDIR/$(basename $0 .zsh).bash
--- setup.py
+++ setup.py
@@ -2,10 +2,14 @@ from setuptools import setup
 
 setup(
 	name="lsl-cli", 
-	version='0.0.1', 
+	version='0.0.2', 
 	entry_points = {
 		'console_scripts': [
 			'lsl = lsl_cli.lsl:main'
 		]
-	}
+	}, 
+	package_data = {
+		'': ['extra/lsl-completion.*']
+	}, 
+	include_package_data = True
 ) 
