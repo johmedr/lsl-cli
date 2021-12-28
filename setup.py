43e1d75c693c312ff0de570b8b4a6bd143da6dc5
Johan Medrano <jmedrano@lirmm.fr>
2021-12-28 18:49:47 +0100

    update setup.py .gitignore

--- setup.py
+++ setup.py
@@ -3,13 +3,22 @@ from setuptools import setup
 setup(
 	name="lsl-cli", 
 	version='0.0.2', 
+	description="Command-line tools for the Lab Streaming Layer.",
+	url="https://github.com/yop0/lsl-cli", 
+	author="Johan Medrano",
+	author_email="johan.medrano653@gmail.com",
+	license="MIT",
 	entry_points = {
 		'console_scripts': [
-			'lsl = lsl_cli.lsl:main'
+			'lsl=lsl_cli.lsl:main'
 		]
 	}, 
+	packages=['lsl_cli'],
+	install_requires = [
+		'pylsl'
+	],
 	package_data = {
 		'': ['extra/lsl-completion.*']
 	}, 
-	include_package_data = True
+	include_package_data = True,
 ) 
