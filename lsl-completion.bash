cb1af1a6d47861ff62371574ce7c578dfcdd25f1
Johan Medrano <jmedrano@lirmm.fr>
2021-12-28 16:42:10 +0100

    Add autocompletion - clean and working

--- lsl-completion.bash
+++ lsl-completion.bash
@@ -1,52 +1,76 @@
 _lsl_completion()
 {
-	local cur prev
+    local cur prev shell_name
+    shell_name=$(ps -p $$ | grep $$ | tr -s ' ' | cut -d ' ' -f 5)
 
+    if [[ $shell_name == "bash" ]]; then
+    	_get_comp_words_by_ref -n : cur
+    fi
+	
 	cur=${COMP_WORDS[COMP_CWORD]}
-	prev=${COMP_WORDS[COMP_CWORD-1]}
 
-	case ${COMP_CWORD} in
-		1)
-            COMPREPLY=($(compgen -W "list echo show find" -- ${cur}))
-			;;
-		2)
-			case ${prev} in 
-				list) _lsl_list_completion ;;
-				echo) _lsl_echo_completion ;;
-				show) _lsl_show_completion ;;
-				find) _lsl_find_completion ;;	
-			esac
-			;;
-		*) 
-			COMPREPLY=()
-			;;
-	esac
-}
+	local COMMANDS=(
+		"list"
+		"echo"
+		"show"
+		"find"
+	)
 
-_lsl_list_completion()
-{
-	COMPREPLY=($(compgen -W "--all --list --timeout --name --type --source_id --channel_count --channel_format --nominal_srate --hostname --uid --version --session_id --created_at"))
-}
+	local command i
+    for (( i=0; i < $COMP_CWORD; i++ )); do
+        if [[ ${COMMANDS[@]} =~ ${COMP_WORDS[i]} ]]; then
+            command=${COMP_WORDS[i]}
+            break
+        fi
+    done
 
-_lsl_echo_completion()
-{
-	_lsl_complete_topics
-}
+    if [[ "$cur" == -* ]]; then
+    	case $command in
+			list) 
+				COMPREPLY=($(compgen -W '--all
+				 	--list --timeout
+				 	--name --type 
+				 	--source_id --channel_count 
+				 	--channel_format --nominal_srate 
+				 	--hostname --uid 
+				 	--version --session_id 
+				 	--created_at' -- ${cur}))
+				return 0
+				;;
+			echo) 
+				COMPREPLY=($(compgen -W "--timeout" -- ${cur}))
+				return 0
+				;;
+			show) 
+				COMPREPLY=($(compgen -W "--timeout" -- ${cur}))
+				return 0
+				;;
+			find) 
+				COMPREPLY=($(compgen -W '
+				 	--name --type 
+				 	--source_id --channel_count 
+				 	--channel_format --nominal_srate 
+				 	--hostname --uid 
+				 	--version --session_id 
+				 	--created_at' -- ${cur}))
+				return 0
+				;;
+		esac
+    fi
 
-_lsl_show_completion()
-{
-	_lsl_complete_topics
-}
+    if [[ -n $command ]]; then
+    	case $command in 
+    		echo|show)
+				COMPREPLY=( $( compgen -W "$( lsl list | xargs echo )" -- ${cur} ) )
+		esac
+	fi
 
-_lsl_find_completion()
-{
-	_lsl_complete_topics
-}
+	if [[ "$command" == "" ]]; then
+		COMPREPLY=( $( compgen -W "$(echo ${COMMANDS[@]})" -- ${cur} ) )
+	fi
 
-_lsl_complete_topics()
-{					
-	COMPREPLY=($(lsl list))
-}
+	return 0
+} &&
 
 
 complete -F _lsl_completion lsl
