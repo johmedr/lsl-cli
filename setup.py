f877b4d285b2d0a2fae956783b00ce0e787e3bde
Johan Medrano <jmedrano@lirmm.fr>
2021-12-28 16:43:48 +0100

    Refactor name

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(
-	name="lslutils", 
+	name="lsl-cli", 
 	version='0.0.1', 
 	entry_points = {
 		'console_scripts': [
-			'lsl = lslutils.lsl:main'
+			'lsl = lsl_cli.lsl:main'
 		]
 	}
 ) 
